2cf7d50dfbeae922e8b4f9bb429db43c4eeae5bb
Thomas Konings <thomasko@suhail.ster.kuleuven.be>
2024-01-04 18:12:03 +0100

    Changing the cgf file to that of WASP-107b

--- vulcan_cfg.py
+++ vulcan_cfg.py
@@ -1,48 +1,49 @@
 # ============================================================================= 
-# Configuration file of VULCAN:  
+# Configuration file of VULCAN: WASP-107b
 # ============================================================================= 
 
 # ====== Setting up the elements included in the network ======
-atom_list = ['H', 'O', 'C', 'N']
+atom_list = ['H', 'O', 'C', 'N', 'S']
 # ====== Setting up paths and filenames for the input and output files  ======
 # input:
-network = 'thermo/NCHO_photo_network.txt'
+network = 'thermo/SNCHO_photo_network.txt'
 use_lowT_limit_rates = False
 gibbs_text = 'thermo/gibbs_text.txt' # (all the nasa9 files must be placed in the folder: thermo/NASA9/)
 cross_folder = 'thermo/photo_cross/'
 com_file = 'thermo/all_compose.txt'
-atm_file = 'atm/atm_HD189_Kzz.txt' # TP and Kzz (optional) file
-sflux_file = 'atm/stellar_flux/sflux-HD189_Moses11.txt' # sflux-HD189_B2020.txt This is the flux density at the stellar surface
-top_BC_flux_file = 'atm/BC_top.txt' # the file for the top boundary conditions
-bot_BC_flux_file = 'atm/BC_bot.txt' # the file for the lower boundary conditions
-vul_ini = 'output/HD189-nominal.vul' # the file to initialize the abundances for ini_mix = 'vulcan_ini'
+atm_file = 'atm/VULCAN_input_files_WASP107b/WASP-107b_picaso_Tint300_10Z_VULCAN.apt' # TP and Kzz (optional) file
+sflux_file = 'atm/stellar_flux/VPL_solar.txt' # sflux-HD189_B2020.txt This is the flux density at the stellar surface
+#top_BC_flux_file = 'atm/BC_top.txt' # the file for the top boundary conditions
+#bot_BC_flux_file = 'atm/BC_bot.txt' # the file for the lower boundary conditions
+#vul_ini = 'output/HD189-nominal.vul' # the file to initialize the abundances for ini_mix = 'vulcan_ini'
 # output:
-output_dir = 'output/'
+output_dir = '../VULCAN_output/WASP-107b/10Z/Tint300/'
 plot_dir = 'plot/'
 movie_dir = 'plot/movie/'
-out_name =  'HD189.vul' # output file name
+out_name =  'WASP-107b_picaso_Tint300_10Z_VULCAN.vul' # output file name
 
 # ====== Setting up the elemental abundance ======
 use_solar = False # True: using the solar abundance from Table 10. K.Lodders 2009; False: using the customized elemental abundance. 
-# customized elemental abundance (only read when use_solar = False)
-O_H = 6.0618E-4 #*(0.793)  
-C_H = 2.7761E-4  
-N_H = 8.1853E-5
-S_H = 1.3183E-5
-He_H = 0.09692
+# customized elemental abundance (only read when use_solar = False) --> Currently set to 10x solar
+O_H = 0.0044789563477    #10x solar
+C_H = 0.0024613695853     #10x solar
+N_H = 0.00061826808643  #10x solar
+S_H = 0.0001205526689    #10x solar
+He_H = 0.07783534053     #10x solar
 ini_mix = 'EQ' # Options: 'EQ', 'const_mix', 'vulcan_ini', 'table' (for 'vulcan_ini, the T-P grids have to be exactly the same)
-fastchem_met_scale = 0.1 # scaling factor for other elements in fastchem (e.g., if fastchem_met_scale = 0.1, other elements such as Si and Mg will take 0.1 solar values)
+fastchem_met_scale = 10 # scaling factor for other elements in fastchem (e.g., if fastchem_met_scale = 0.1, other elements such as Si and Mg will take 0.1 solar values)
 
 # Initialsing uniform (constant with pressure) mixing ratios (only reads when ini_mix = const_mix)
-const_mix = {'CH4':2.7761E-4*2, 'O2':4.807e-4, 'He':0.09691, 'N2':8.1853E-5, 'H2':1. -2.7761E-4*2*4/2} 
+#const_mix = {'CH4':2.7761E-4*2, 'O2':4.807e-4, 'He':0.09691, 'N2':8.1853E-5, 'H2':1. -2.7761E-4*2*4/2} 
 
 # ====== Setting up photochemistry ======
 use_photo = True
 # astronomy input
-r_star = 0.805 # stellar radius in solar radius
-Rp = 1.138*7.1492E9 # Planetary radius (cm) (for computing gravity)
-orbit_radius = 0.03142 # planet-star distance in A.U.
-sl_angle = 48 /180.*3.14159 # the zenith angle of the star in degree (usually 58 deg for the dayside average)
+
+r_star = 0.66 # stellar radius in solar radius
+Rp = 10.54 * 6.378E8 # Planetary radius (cm) (for computing gravity)
+orbit_radius = 0.05500 # planet-star distance in A.U.
+sl_angle = 58 /180.* 3.14159 # the zenith angle of the star in degree (usually 58 deg for the dayside average)
 f_diurnal = 1. # to account for the diurnal average of solar flux (i.e. 0.5 for Earth; 1 for tidally-locked planets) 
 scat_sp = ['H2', 'He'] # the bulk gases that contribute to Rayleigh scattering
 T_cross_sp = [] # warning: slower start! available atm: 'CO2','H2O','NH3', 'SH','H2S','SO2', 'S2', 'COS', 'CS2'
@@ -66,23 +67,27 @@ if use_photo == False and use_ion == True:
 # ====== Setting up parameters for the atmosphere ======
 atm_base = 'H2' #Options: 'H2', 'N2', 'O2', 'CO2 -- the bulk gas of the atmosphere: changes the molecular diffsion, thermal diffusion factor, and settling velocity
 rocky = False # for the surface gravity
-nz = 150   # number of vertical layers
-P_b = 1e9  # pressure at the bottom (dyne/cm^2)
-P_t = 1e-2 # pressure at the top (dyne/cm^2)
+nz = 150  # number of vertical layers
+P_b = 3e2 * 1e6 # pressure at the bottom (dyne/cm^2)
+P_t = 1e-7 * 1e6 # pressure at the top (dyne/cm^2)
 use_Kzz = True
 use_moldiff = True
 use_vz = False
+
 atm_type = 'file'  # Options: 'isothermal', 'analytical', 'file', or 'vulcan_ini' 'table'
-Kzz_prof = 'file' # Options: 'const','file' or 'Pfunc' (Kzz increased with P^-0.4)
-K_max = 1e5        # for Kzz_prof = 'Pfunc'
-K_p_lev = 0.1      # for Kzz_prof = 'Pfunc'
+Kzz_prof = 'const' # Options: 'const','file' or 'Pfunc' (Kzz increased with P^-0.4)
+
+#K_max = 1e5        # for Kzz_prof = 'Pfunc'
+#K_p_lev = 0.1      # for Kzz_prof = 'Pfunc'
 vz_prof = 'const'  # Options: 'const' or 'file'
-gs = 2140.         # surface gravity (cm/s^2)  (HD189:2140  HD209:936)
-Tiso = 1000 # only read when atm_type = 'isothermal'
+gs = 269.28         # surface gravity (cm/s^2)  (HD189:2140  HD209:936)
+
+#Tiso = 1000 # only read when atm_type = 'isothermal'
+
 # setting the parameters for the analytical T-P from (126)in Heng et al. 2014. Only reads when atm_type = 'analytical' 
 # T_int, T_irr, ka_L, ka_S, beta_S, beta_L
-para_warm = [120., 1500., 0.1, 0.02, 1., 1.]
-para_anaTP = para_warm
+#para_warm = [120., 1500., 0.1, 0.02, 1., 1.]
+#para_anaTP = para_warm
 const_Kzz = 1.E10 # (cm^2/s) Only reads when use_Kzz = True and Kzz_prof = 'const'
 const_vz = 0 # (cm/s) Only reads when use_vz = True and vz_prof = 'const'
 
@@ -138,13 +143,12 @@ slope_cri = 1.e-4
 yconv_min = 0.1
 flux_cri = 0.1
 flux_atol = 1. # the tol for actinc flux (# photons cm-2 s-1 nm-1)
-conver_ignore = [] # added 2023. to get rid off non-convergent species, e.g. HC3N without sinks 
 
 # ====== Setting up numerical parameters for Ros2 ODE solver ====== 
 rtol = 0.2             # relative tolerence for adjusting the stepsize 
 post_conden_rtol = 0.1 # switched to this value after fix_species_time
 
-# ====== Setting up for ouwtput and plotting ======
+# ====== Setting up for output and plotting ======
 # plotting:
 plot_TP = False
 use_live_plot = True
@@ -158,7 +162,7 @@ use_PIL = True
 live_plot_frq = 10
 save_movie_rate = live_plot_frq
 y_time_freq = 1  #  storing data for every 'y_time_freq' step
-plot_spec = ['H2O', 'H', 'CH4', 'CO', 'CO2', 'C2H2', 'HCN', 'NH3' ]
+plot_spec = ['H2O', 'H', 'CH4', 'CO', 'CO2', 'C2H2', 'HCN', 'NH3', 'SO2' ]
 # output:
 output_humanread = False
 use_shark = False
